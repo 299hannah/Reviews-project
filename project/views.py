73bf76b1125c35ccbeab129fe7002a89e4d7e6fa
299hannah <hannah.wambui@student.moringaschool.com>
2021-07-18 10:14:26 +0300

    My skeleton project with homepage

---
+++ project/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
+
+# Create your views here.
+
+def index(request):
+	return render(request, "index.html")
+
+
+
+
