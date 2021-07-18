8c159d6e921abb60033a407ae775a23b4532e996
299hannah <hannah.wambui@student.moringaschool.com>
2021-07-18 23:47:28 +0300

    my profile form

--- project/views.py
+++ project/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render, redirect
-from .forms import UserCreationForm
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django_registration.backends.one_step.views import RegistrationView
+from django.contrib.auth.models import User
+from .forms import UserProfileForm
 from django.contrib import messages
-
+from .models import UserProfile
 # Create your views here.
 
 def index(request):
 	return render(request, "index.html")
 
-# def registration_page(request):
-#     # 'form'=UserCreationForm
-#     # ctx={
-
-#     # }
-#     return render(request, 'registration/register.html')
+def profile_view(request):
+    logged_user=request.user
+    user=UserProfile.objects.get(user=logged_user)
+    
+    print('hellooo.............',user)
+    ctx={
+        'user': user
+    }
+    return render(request, 'profile.html', ctx)
 
 
 
